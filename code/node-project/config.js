3344d61904ae08e6b0bb18371c33a32dfee3b8a7
Cristian Chicaiza <53982083+crisschic@users.noreply.github.com>
2020-10-02 13:48:56 -0500

    APP estable
    falta modificar

--- code/node-project/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.sfnjn.gcp.mongodb.net/utups?retryWrites=true&w=majority',
-    port: process.env.PORT || 5000,
-    host: process.env.HOST || 'http://localhost',
-    publicRoute: process.env.PUBLIC_ROUTE || '/',
-    filesRoute: process.env.FILES_ROUTE || 'files',
-}
-
-module.exports = config
